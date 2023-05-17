57581dcb9d65235a8b0bdaae2a58cb0410f319c0
Mahmoud Ibrahiam <equalmahmoud@gmail.com>
2023-05-17 14:42:22 +0300

    fix connection error

--- src/config/dbConnect.ts
+++ src/config/dbConnect.ts
@@ -2,10 +2,11 @@ import mongoose, { ConnectOptions } from "mongoose";
 
 export default async function dbConnect() {
     try {
-        if (mongoose.connections[0].readyState) return;
+        mongoose.set("strictQuery", false);
         await mongoose.connect(process.env.MONGODB_URI!, {
             useUnifiedTopology: true,
             useNewUrlParser: true,
+            family: 4,
         } as ConnectOptions);
     } catch (error) {
         console.log(error);

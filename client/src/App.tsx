6c46e97b86975a97828a4e30bb1f1601bffe5668
Mahmoud Ibrahiam <equalmahmoud@gmail.com>
2023-05-17 15:44:37 +0300

    pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,18 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Auth, Home } from "@pages/index";
+import PrivateRoute from "@components/PrivateRoute";
+
 function App() {
-    return <div></div>;
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<PrivateRoute />}>
+                    <Route index path="/" element={<Home />} />
+                </Route>
+                <Route path="/auth" element={<Auth />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;

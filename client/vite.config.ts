f08c0f2f8ffcbfcbac1943a87a50955f553d1fbb
Mahmoud Ibrahiam <equalmahmoud@gmail.com>
2023-05-17 15:21:21 +0300

    absolute imports config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    server: {
+        port: 3000,
+        host: true,
+        proxy: {
+            "/api": {
+                target: "http://localhost:5000/api",
+                changeOrigin: true,
+                secure: false,
+                rewrite: (path) => path.replace("/api", ""),
+            },
+        },
+    },
+    resolve: {
+        alias: {
+            "@src": path.resolve(__dirname, "src"),
+            "@components": path.resolve(__dirname, "src/components"),
+            "@hooks": path.resolve(__dirname, "src/hooks"),
+            "@pages": path.resolve(__dirname, "src/pages"),
+        },
+    },
+});
